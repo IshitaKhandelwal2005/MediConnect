1635045143f49fae4fc152ad852d0086ebb66c87
IshitaKhandelwal2005 <153984063+IshitaKhandelwal2005@users.noreply.github.com>
2025-02-05 00:31:19 +0530

    Update MyAppointments.jsx

--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -79,7 +79,7 @@ const MyAppointments =()=> {
               </div>
               <div></div>
               <div className='flex flex-col gap-2 justify-end'>
-                  {!item.cancelled && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-[#1D2129] hover:text-white transition-all duration-300'>Pay Online</button>}
+                  {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-[#1D2129] hover:text-white transition-all duration-300'>Pay Online</button>}
                   {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-[#1D2129] hover:text-white transition-all duration-300'>Cancel Appointment</button>}
                   {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
               </div>
